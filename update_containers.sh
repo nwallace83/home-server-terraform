b5f668ff9ba44bb2752b6504d4e22ed0911c230a
Nathan Wallace <nathan.wallace114@gmail.com>
2023-09-01 19:16:21 -0600

    add shell script to audo-update server

---
+++ update_containers.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -x
+
+PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+cd $SCRIPT_DIR
+
+docker pull linuxserver/sickchill:latest
+docker pull linuxserver/radarr:latest
+docker pull nginx:latest
+docker pull linuxserver/prowlarr:latest
+docker pull plexinc/pms-docker:latest
+docker pull binhex/arch-delugevpn:latest
+docker pull jlesage/handbrake:latest
+docker image prune -f
+
+git pull
+
+terraform destroy -auto-approve
+terraform plan
+terraform apply -auto-approve
