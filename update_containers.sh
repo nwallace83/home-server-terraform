3f252b3c24381671c80671696cb55f621a2889bd
Nathan Wallace <nathan.wallace114@gmail.com>
2023-09-05 19:57:55 -0600

    update update script

--- update_containers.sh
+++ update_containers.sh
@@ -2,26 +2,18 @@
 
 echo "Start time: `date`"
 
-PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/snap/bin
+
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
 cd $SCRIPT_DIR
 
-docker pull linuxserver/sickchill:latest
-docker pull linuxserver/radarr:latest
-docker pull nginx:latest
-docker pull linuxserver/prowlarr:latest
-docker pull plexinc/pms-docker:latest
-docker pull binhex/arch-delugevpn:latest
-docker pull jlesage/handbrake:latest
-
-git checkout .terraform.lock.hcl
-git pull
-
-terraform init --upgrade -force-copy
-terraform destroy -auto-approve
-terraform plan
-terraform apply -auto-approve
-
-docker image prune -f
+kubectl rollout restart deployment handbrake
+kubectl rollout restart deployment sickchill
+kubectl rollout restart deployment delugevpn
+kubectl rollout restart deployment plex
+kubectl rollout restart deployment pihole
+kubectl rollout restart deployment prowlarr
+kubectl rollout restart deployment radarr
 
 echo "End time: `date`"
