13a06cd182005874073832d5ded0180967a16501
Nathan Wallace <nathan.wallace114@gmail.com>
2023-09-11 22:38:20 -0600

    trying shit

--- pihole/ingress.tf
+++ pihole/ingress.tf
@@ -39,23 +39,5 @@ resource "kubernetes_ingress_v1" "pihole_ingress" {
         }
       }
     }
-
-    rule {
-      http {
-        path {
-          path      = "/(.*)"
-          path_type = "Prefix"
-          backend {
-            service {
-              name = kubernetes_service.pihole.metadata.0.name
-
-              port {
-                number = 80
-              }
-            }
-          }
-        }
-      }
-    }
   }
 }
